1e67696b05ed89139ea6dc2d2ba4b811bf8c4806
carterdealma <85889648+carterdealma@users.noreply.github.com>
2024-02-02 22:11:11 -0700

    Initial Commit

---
+++ Cat/CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatApp.swift
+//  Cat
+//
+//  Created by CARTER MA on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
